1635a7bcfba3592469bc64f48c594436559200ce
mvalnet <milla.valnet@lip6.fr>
2026-02-18 15:18:41 +0100

    Initial commit

---
+++ conf.py
@@ -0,0 +1,27 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'Chamelon'
+copyright = '2026, Milla Valnet, Nathanaëlle Courant, Basile Clément, Guillaume Bury, Pierre Chambart, Vincent Laviron'
+author = 'Milla Valnet, Nathanaëlle Courant, Basile Clément, Guillaume Bury, Pierre Chambart, Vincent Laviron'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = []
+
+templates_path = ['_templates']
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'sphinx_rtd_theme'
+html_static_path = ['_static']
---
+++ pages/installation.rst
@@ -0,0 +1,22 @@
+Installation
+============
+
+Downloading Chamelon
+-----------------
+
+The source code of Chamelon can be found at `<https://github.com/Ekdohibs/chamelon>`_.
+Use Git to get access to the latest version:
+
+.. code-block:: shell-session
+
+   $ git clone https://github.com/Ekdohibs/chamelon.git
+
+Installing Chamelon from source
+----------------------------
+
+Make sure you have an ``opam switch`` with compiler Ocaml 4.14.0 working on your machine. Instructions to create this switch can be found at `<https://ocaml.org/docs/opam-switch-introduction>`_. Then simply run:
+
+.. code-block:: shell-session
+
+   $ dune build
+
